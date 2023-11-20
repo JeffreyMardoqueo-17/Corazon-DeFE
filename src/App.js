1651540c1016dc507c370e6f3dee12b32e822a0f
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-11-19 21:22:56 -0600

    CARRUSEL

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Menu from './Components/Menu.jsx'
+import { Carrusel } from './Components/Carrusel.jsx';
 function App() {
   return (
     <div className="App">
       <Menu />
+      <Carrusel></Carrusel>
     </div>
   );
 }
