a264fb3c17157fa9297e5e482a6ef6ec63ec1471
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-12-09 22:41:00 -0600

    section fill

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
 function Cantos({ imageUrl, title, description, videoUrl }) {
-  return (
-    <div className='container'>
-      <div className="card bg-dark" style={{ width: '18rem' }}>
-        <img src={imageUrl} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title text-white font-weight-bold">{title}</h5>
-          <p className="card-text text-white">{description}</p>
-          <a href={videoUrl} className="btn btn-primary">Ver video</a>
+    return (
+        <div className='container'>
+            <div className="card bg-dark" style={{ width: '18rem' }}>
+                <img src={imageUrl} className="card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title text-white font-weight-bold">{title}</h5>
+                    <p className="card-text text-white">{description}</p>
+                    <a href={videoUrl} className="btn btn-primary">Ver video</a>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default Cantos;
