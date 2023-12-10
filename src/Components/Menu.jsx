28766b92c114ac64a0736d3cd34df0c598d8fe79
Jeffrey Mardoqueo <jeffreymardoqueo260@gmail.com>
2023-12-10 06:28:43 -0600

    section de rutas con menu actualizado

--- src/Components/Menu.jsx
+++ src/Components/Menu.jsx
@@ -1,80 +1,89 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';  // Asegúrate de importar Link de react-router-dom
+
 import './menu.css';
-//aqui para las rutas
-import { Link } from 'react-router-dom';
-import App from '../App';
 
 const Cabecera = () => {
+    // Actualiza las rutas para que coincidan con los IDs de las secciones
     const list = [
         {
             text: 'Sobre nosotros',
-            link: 'sobreElGrupo',
+            link: '#sobreElGrupo',
         },
         {
             text: 'Sobre Jeffrey',
-            link: '/aboutJeff',
+            link: '#aboutJeff',
         },
         {
             text: 'Sobre Set',
-            link: '/sobreset',
+            link: '#sobreset',
         },
         {
             text: 'Cantos',
-            link: '/cantos',
+            link: '#cantos',
         },
         {
             text: 'Contacto',
-            link: '/footer',
+            link: '#footer',
         },
         {
-            text: 'random',
-            link: '/random',
+            text: 'Random',
+            link: '#random',
         },
     ];
 
-
     const [menu, setMenu] = useState(false);
 
-    const toggleMenu = () => {
-        setMenu(!menu);
+    // Función para manejar el clic en los enlaces y realizar un desplazamiento suave
+    const handleLinkClick = (event, targetId) => {
+        event.preventDefault();
 
-        // Agrega o quita la clase al body para habilitar/deshabilitar el scroll
-        if (!menu) {
-            document.body.classList.add('no-scroll');
-        } else {
-            document.body.classList.remove('no-scroll');
+        const targetElement = document.getElementById(targetId);
+        if (targetElement) {
+            targetElement.scrollIntoView({ behavior: 'smooth' });
         }
-    };
 
-    const handleLinkClick = () => {
+        // Cierra el menú y restaura el desplazamiento del cuerpo
         setMenu(false);
         document.body.classList.remove('no-scroll');
     };
 
+    // Renderiza el componente
     return (
         <header className="Cabecera">
             <h1 className="Cabecera-h1">
-                <a href="#" className="Cabecera-a">
+                {/* Utiliza Link para el logo */}
+                <Link to="/" className="Cabecera-a">
                     JS
-                </a>
+                </Link>
             </h1>
 
             <button
-                onClick={toggleMenu}
+                onClick={() => {
+                    setMenu(!menu);
+                    // Agrega o quita la clase al body para habilitar/deshabilitar el scroll
+                    document.body.classList.toggle('no-scroll');
+                }}
                 className="Cabecera-button"
             >
                 <svg className='Cabecera-svg' xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
+                    <path fillRule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                 </svg>
             </button>
+
             <nav className={`Cabecera-nav ${menu ? 'isActive' : ''}`}>
                 <ul className="Cabecera-ul">
+                    {/* Utiliza el método map para renderizar la lista de enlaces */}
                     {list.map((item, index) => (
                         <li key={index} className="Cabecera-li">
                             {/* Utiliza el componente Link en lugar de a */}
-                            <Link to={item.link} className="Cabecera-a" onClick={handleLinkClick}>
+                            <a
+                                href={item.link}
+                                className="Cabecera-a"
+                                onClick={(e) => handleLinkClick(e, item.link.substring(1))}
+                            >
                                 <span className="title">{item.text}</span>
-                            </Link>
+                            </a>
                         </li>
                     ))}
                 </ul>
